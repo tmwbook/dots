0b05b967302ccdffdcd62661d4143104de14ad1a
Tom White <tmwbook@gmail.com>
2026-04-18 21:04:29 -0400

    nvim: LSP folder - ruff, basedpyright

---
+++ nvim/.config/nvim/lsp/ruff.lua
@@ -0,0 +1,19 @@
+---@type vim.lsp.Config
+return {
+	cmd = { "ruff", "server" },
+	filetypes = { "python" },
+	root_markers = { "pyproject.toml", "ruff.toml", ".ruff.toml", ".git" },
+	--[[ init_options = {
+		settings = {
+			-- Ruff language server settings go here
+		},
+	}, ]]
+	on_attach = function(client, bufnr)
+		client.server_capabilities.hoverProvider = false
+	end,
+	capabilities = {
+		general = {
+			positionEncodings = { "utf-16" },
+		},
+	},
+}
