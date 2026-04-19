deb82124af6ac56048246c1a83257c779606a1e8
Tom White <tmwbook@gmail.com>
2026-04-18 21:01:21 -0400

    nvim: autopairs, neo-tree

---
+++ nvim/.config/nvim/lua/custom/plugins/neo-tree.lua
@@ -0,0 +1,36 @@
+-- Neo-tree is a Neovim plugin to browse the file system
+-- https://github.com/nvim-neo-tree/neo-tree.nvim
+
+return {
+	"nvim-neo-tree/neo-tree.nvim",
+	version = "*",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+	},
+	cmd = "Neotree",
+	keys = {
+		{ "\\", ":Neotree reveal<CR>", { desc = "NeoTree reveal" } },
+	},
+	opts = {
+		filesystem = {
+			window = {
+				mappings = {
+					["\\"] = "close_window",
+				},
+			},
+		},
+		event_handlers = {
+			{
+				event = "file_open_requested",
+				handler = function()
+					-- auto close
+					-- vim.cmd("Neotree close")
+					-- OR
+					require("neo-tree.command").execute({ action = "close" })
+				end,
+			},
+		},
+	},
+}
